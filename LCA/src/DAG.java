e67f13b51ab907930bc355a7764fab92acd3a203
patrickbyrne <patrickbyrne@172.20.10.2>
2019-10-14 14:18:03 +0100

    function that creates an empty graph with a chosen size DAG

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -1,4 +1,38 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+
 
 public class DAG {
+	private int V;			         	// Vertices		
+	private int E;					    // Edges
+	private ArrayList<Integer>[] adj;    //adj[V] = adjacency list for vertex V
+	private int [] indegree;			    // indegree of vertices or edges
+	private int [] outdegree;			// outdegree of vertices or edges
+	private boolean marked [];			// if a vertices has been used
+	private boolean hasCycle;			// true or false for cycle
+	private boolean stack [];			
+	
+	
+	public DAG(int V) //Implementation of graph with size v
+	{
+		if(V < 0)
+		{
+			throw new IllegalArgumentException("Number of vertices must be greater than 0");
+		}
+		
+		this.V = V;
+		this.E = 0;
+		indegree = new int[V];
+		marked = new boolean[V];
+		stack = new boolean[V];
+		adj = (ArrayList<Integer>[]) new ArrayList[V];
+		
+		for(int v = 0; v < V; v++)
+		{
+			adj[v] = new ArrayList<Integer>();
+		}
+	}
 
 }
