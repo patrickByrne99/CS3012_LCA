91441ce3ed91b5910eb93105862759c4722fb8c6
patrickbyrne <patrickbyrne@172.20.10.2>
2019-10-15 10:11:57 +0100

    Testing the DAG, adding edges,Empty LCA and the in/out degree

--- LCA/src/DAGTest.java
+++ LCA/src/DAGTest.java
@@ -1,9 +1,39 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-public class DAGTest
-{
+public class DAGTest {
+
+	@Test
+	public void testAddEdge(){
+		DAG LCA = new DAG(5);
+		LCA.addEdge(0, 1);
+		LCA.addEdge(-2, -5);
+		
+		assertEquals(1, LCA.E()); //if Edge count is 1
+		
+		LCA.addEdge(1, 2);
+		
+		assertEquals(2, LCA.E()); //Two edges
+	}
+
+	// each new DAG called LCA in each test is different
 	
+	@Test
+	public void testLCAEmpty() { 
+		DAG LCA = new DAG(10);
+		assertEquals(-1, LCA.findLCA(1, 2)); 
+	}
 	
+	@Test
+	public void testinDegree(){
+		DAG LCA = new DAG(5);
+		assertEquals(-1, LCA.indegree(-3));
+	}
+	
+	@Test
+	public void testOutDegree(){
+		DAG LCA = new DAG(5);
+		assertEquals(-1, LCA.outdegree(8));	
+	}
+
 }
