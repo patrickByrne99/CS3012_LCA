ea08d2e799e549eed6b84abd0dfb4fe068a377fd
patrickbyrne <patrickbyrne@172.20.10.2>
2019-10-16 14:24:08 +0100

    adding tests for, edges vertices and the directed graphs its self

--- LCA/src/DAGTest.java
+++ LCA/src/DAGTest.java
@@ -4,6 +4,40 @@ import org.junit.Test;
 public class DAGTest {
 
 	@Test
+	public void testDAG(){
+		DAG test = new DAG(10);
+		test.addEdge(1, 2);
+		test.addEdge(1, 3);
+		test.addEdge(3, 4);
+		test.addEdge(4, 5);
+		test.addEdge(4, 6);
+		
+		assertEquals(1, test.indegree(4));
+		assertEquals(2, test.outdegree(4));
+		assertEquals(5, test.E());
+		assertEquals(10, test.V());
+		String ans = "[5, 6]";
+		assertEquals(ans, test.adj(4).toString());
+	}
+	@Test 
+	public void testV()
+	{
+		DAG graph = new DAG(6);
+		assertEquals(6, graph.V());
+	}
+	
+	@Test
+	public void testE(){
+		
+		DAG graph = new DAG(5);
+		
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 4);
+		graph.addEdge(3, 3);
+		
+		assertEquals(3, graph.E());
+	}
+	@Test
 	public void testAddEdge(){
 		DAG LCA = new DAG(5);
 		LCA.addEdge(0, 1);
@@ -16,8 +50,15 @@ public class DAGTest {
 		assertEquals(2, LCA.E()); //Two edges
 	}
 
-	// each new DAG called LCA in each test is different
+	@Test(expected=Exception.class)
+	public void exceptionTest(){
+		
+		//Can't make a directed graph with less than 0 vertices
+		
+		DAG LCA = new DAG(-5);
+	}
 	
+	// each new DAG called LCA in each test is different
 	@Test
 	public void testLCAEmpty() { 
 		DAG LCA = new DAG(10);
